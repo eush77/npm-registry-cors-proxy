6de7842c2f0cb0c6d37029cb92bdf9d23988d6c3
Eugene Sharygin <eush77@gmail.com>
2015-03-23 13:02:29 +0300

    Use strict and url.resolve

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
+'use strict';
 
 var request = require('request'),
     cors = require('cors'),
-    express = require('express');
+    express = require('express'),
+    urlResolve = require('url').resolve;
+
 
 var app = express();
 
-app.get('/:module', cors(), function(req, res){
-  var url = "http://registry.npmjs.org/" + req.params.module
-  request.get({url:url, json:true}, function(err, resp, body) {
+app.get('/:module', cors(), function (req, res){
+  var url = urlResolve('http://registry.npmjs.org/', req.params.module);
+  request.get({ url: url, json: true }, function (err, resp, body) {
     res.jsonp(body);
   });
 });
